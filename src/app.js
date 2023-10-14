9b1112376ba112d9ffa35a2d9f426d6bcb6f939c
wes337 <wesmoses@gmail.com>
2023-10-14 20:16:25 +0300

    filename case changes

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import Backdrop from "./components/backdrop";
+import Footer from "./components/footer";
+import TopBar from "./components/top-bar";
+import Home from "./pages/home";
+import Work from "./pages/work";
+import Contact from "./pages/contact";
+import Shop from "./pages/shop";
+import More from "./pages/more";
+import "./styles/app.scss";
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <>
+      <Backdrop />
+      <TopBar />
+      <Routes location={location} key={location.pathname}>
+        <Route index element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/more" element={<More />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
